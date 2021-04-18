a6397f543d4dcb2b8d6f4a17ffcbd6af627b21e3
unknown <vishalpoddar2468@gmail.com>
2021-04-18 15:12:47 +0530

    added rupees symbol in details page

--- src/ProductDetailInfo.js
+++ src/ProductDetailInfo.js
@@ -5,7 +5,7 @@ export default function ProductDetailInfo({ product, onProductAdd }) {
   return (
     <>
       <p>
-        {product.description} sold at <strong>${product.price}</strong> per
+        {product.description} sold at <strong>₹{product.price}</strong> per
         piece.
       </p>
       <Button onClick={() => onProductAdd(product)}>${product.price}</Button>
